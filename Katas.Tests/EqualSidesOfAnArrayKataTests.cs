47abda161bf903c3dfdb80ec722f052e702d479f
Eddy Fidel <eddy.fidel0809@gmail.com>
2018-02-28 14:50:46 -0400

    EqualSidesOfAnArrayKata has been added

---
+++ Katas.Tests/EqualSidesOfAnArrayKataTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Katas.Tests
+{
+    [TestFixture]
+    public class EqualSidesOfAnArrayKataTests
+    {
+        [Test]
+        public void Test1() 
+        {
+            Assert.AreEqual(3, EqualSidesOfAnArrayKata.FindEvenIndex(new int[] { 1, 2, 3, 4, 3, 2, 1 }));
+            Assert.AreEqual(1, EqualSidesOfAnArrayKata.FindEvenIndex(new int[] { 1, 100, 50, -51, 1, 1 }));
+            Assert.AreEqual(-1, EqualSidesOfAnArrayKata.FindEvenIndex(new int[] { 1, 2, 3, 4, 5, 6 }));
+            Assert.AreEqual(3, EqualSidesOfAnArrayKata.FindEvenIndex(new int[] { 20, 10, 30, 10, 10, 15, 35 }));
+        }
+    }
+}
---
+++ Katas/EqualSidesOfAnArrayKata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Katas
+{
+    public static class EqualSidesOfAnArrayKata
+    {
+        public static int FindEvenIndex(int[] arr)
+        {
+            int evenIndex = -1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int[] leftSideArray = arr.Where((a, j) => j < i).ToArray();
+
+                int[] rightSideArray = arr.Where((a, j) => j > i).ToArray();
+
+                int leftSum = leftSideArray.Sum(x => x);
+
+                int rightSum = rightSideArray.Sum(x => x);
+
+                if (leftSum == rightSum) 
+                {
+                    return i;
+                }
+            }
+
+            return evenIndex;
+        }
+    }
+}
