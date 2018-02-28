a9c852370b186c562f2459fff24f559fd66292ca
Eddy Fidel <eddy.fidel0809@gmail.com>
2018-02-28 11:22:51 -0400

    OnesAndZerosKata has been added

---
+++ Katas.Tests/OnesAndZerosKataTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Katas.Tests
+{   
+    [TestFixture]
+    public class OnesAndZerosKataTests
+    {
+        int[] array1 = new int[] { 0, 0, 0, 0 };
+        int[] array2 = new int[] { 1, 1, 1, 1 };
+        int[] array3 = new int[] { 0, 1, 1, 0 };
+        int[] array4 = new int[] { 0, 1, 0, 1 };
+
+        [Test]
+        public void Test1() 
+        {
+            Assert.AreEqual(0, OnesAndZerosKata.BinaryArrayToNumber(array1));
+            Assert.AreEqual(15, OnesAndZerosKata.BinaryArrayToNumber(array2));
+            Assert.AreEqual(6, OnesAndZerosKata.BinaryArrayToNumber(array3));
+            Assert.AreEqual(5, OnesAndZerosKata.BinaryArrayToNumber(array4));
+        }
+    }
+}
---
+++ Katas/OnesAndZerosKata.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Katas
+{
+    public static class OnesAndZerosKata
+    {
+        public static int BinaryArrayToNumber(int[] binaryArray)
+        {
+            int number = 0;
+
+            int count = binaryArray.Length - 1;
+
+            foreach (var binary in binaryArray)
+            {
+                if (binary == 1)
+                {
+                    number += (int)Math.Pow(2, count);
+                }
+
+                count--;
+            }
+
+            return number;
+        }
+    }
+}
