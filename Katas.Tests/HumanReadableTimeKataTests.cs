5d839d42ad431d30769fd70bfa492e3f95b782bd
Eddy Fidel <eddy.fidel0809@gmail.com>
2018-03-01 14:32:41 -0400

    HumanReadableTimeKata has been added

---
+++ Katas.Tests/HumanReadableTimeKataTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Katas.Tests
+{
+    [TestFixture]
+    public class HumanReadableTimeKataTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(HumanReadableTimeKata.GetReadableTime(0), "00:00:00");
+            Assert.AreEqual(HumanReadableTimeKata.GetReadableTime(5), "00:00:05");
+            Assert.AreEqual(HumanReadableTimeKata.GetReadableTime(60), "00:01:00");
+            Assert.AreEqual(HumanReadableTimeKata.GetReadableTime(86399), "23:59:59");
+            Assert.AreEqual(HumanReadableTimeKata.GetReadableTime(359999), "99:59:59");
+        }
+    }
+}
---
+++ Katas/HumanReadableTimeKata.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Katas
+{
+    public static class HumanReadableTimeKata
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            int minutes = 0, 
+                hours = 0;
+
+            if (seconds > 59)
+            {
+                minutes = (int)Math.Floor((decimal)(seconds / 60));
+
+                seconds = seconds - (minutes * 60);
+
+                if (minutes > 59)
+                {
+                    hours = (int)Math.Floor((decimal)(minutes / 60));
+
+                    minutes = minutes - (hours * 60);
+                }
+            }
+
+            string dateString = string.Format("{0}:{1}:{2}", 
+                                              hours.ToString().PadLeft(2, '0'), 
+                                              minutes.ToString().PadLeft(2, '0'), 
+                                              seconds.ToString().PadLeft(2, '0'));
+
+            return dateString;
+        }
+    }
+}
