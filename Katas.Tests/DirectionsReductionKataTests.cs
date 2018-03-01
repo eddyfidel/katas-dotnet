65f6e10fc04c9561080cf9bd613752e0787eb684
Eddy Fidel <eddy.fidel0809@gmail.com>
2018-03-01 17:08:34 -0400

    DirectionsReductionKata has been added

---
+++ Katas.Tests/DirectionsReductionKataTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Katas.Tests
+{
+    [TestFixture]
+    public class DirectionsReductionKataTests
+    {
+        [Test]
+        public void Test1()
+        {
+            string[] a = new string[] { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+            string[] b = new string[] { "WEST" };
+            Assert.AreEqual(b, DirectionsReductionKata.DirReduc(a));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            string[] a = new string[] { "NORTH", "WEST", "SOUTH", "EAST" };
+            string[] b = new string[] { "NORTH", "WEST", "SOUTH", "EAST" };
+            Assert.AreEqual(b, DirectionsReductionKata.DirReduc(a));
+        }
+    }
+}
---
+++ Katas/DirectionsReductionKata.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+
+namespace Katas
+{
+    public class DirectionsReductionKata
+    {
+        public static string[] DirReduc(string[] arr)
+        {
+            bool hasReduction = false;
+
+            int firstPosition = 0, 
+                secondPosition = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i != arr.Length - 1)
+                {
+                    bool hasNorthSouthReduction = (arr[i] == "NORTH" && arr[i + 1] == "SOUTH") 
+                        || (arr[i + 1] == "NORTH" && arr[i] == "SOUTH");
+
+                    bool hasEastWestReduction = (arr[i] == "EAST" && arr[i + 1] == "WEST")
+                        || (arr[i + 1] == "EAST" && arr[i] == "WEST");
+
+                    if (hasNorthSouthReduction || hasEastWestReduction)
+                    {
+                        firstPosition = i;
+
+                        secondPosition = i + 1;
+
+                        hasReduction = true;
+
+                        break;
+                    }
+                }
+            }
+
+            if (arr.Length <= 1 || (arr.Length == 2 && !hasReduction))
+            {
+                return arr;
+            }
+
+            if (hasReduction)
+            {
+                arr = arr.Where((a, i) => i != firstPosition 
+                                && i != secondPosition).ToArray();
+
+                return DirReduc(arr);
+            }
+
+            return arr;
+        }
+    }
+}
