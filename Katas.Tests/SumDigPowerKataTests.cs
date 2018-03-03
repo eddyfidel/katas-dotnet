f7102a0ad53d2663d75f501779049dcede4b61be
Eddy Fidel <eddy.fidel0809@gmail.com>
2018-03-03 11:59:18 -0400

    SumDigPowerKata has been added

---
+++ Katas.Tests/SumDigPowerKataTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace Katas.Tests
+{
+    [TestFixture]
+    public class SumDigPowerKataTests
+    {
+        private static string Array2String(long[] list)
+        {
+            return "[" + string.Join(", ", list) + "]";
+        }
+
+        private static void testing(long a, long b, long[] res)
+        {
+            Assert.AreEqual(Array2String(res),
+                            Array2String(SumDigPowerKata.SumDigPow(a, b)));
+        }
+
+        [Test]
+        public static void Test1()
+        {
+            testing(1, 10, new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            testing(1, 100, new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 89 });
+            testing(10, 100, new long[] { 89 });
+            testing(90, 100, new long[] { });
+            testing(90, 150, new long[] { 135 });
+            testing(50, 150, new long[] { 89, 135 });
+            testing(10, 150, new long[] { 89, 135 });
+
+        }
+    }
+}
---
+++ Katas/SumDigPowerKata.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Katas
+{
+    public class SumDigPowerKata
+    {
+        public static long[] SumDigPow(long a, long b)
+        {
+            List<long> powNumbers = new List<long>();
+
+            for (long i = a; i <= b; i++)
+            {
+                int number = 0;
+
+                int powCount = 1;
+
+                var digits = i.ToString().Select(d => Char.GetNumericValue(d));
+
+                foreach (var digit in digits)
+                {
+                    number += (int)Math.Pow(digit, powCount);
+
+                    powCount++;
+                }
+
+                if (number == i)
+                {
+                    powNumbers.Add(number);
+                }
+            }
+
+            return powNumbers.ToArray();
+        }
+    }
+}
